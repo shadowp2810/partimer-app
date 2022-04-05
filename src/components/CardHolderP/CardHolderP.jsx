b164497f363f30fe72a3d6f224edf3231f74fc1b
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2022-04-06 00:44:05 +0530

    Add search filter feature.

--- src/components/CardHolderP/CardHolderP.jsx
+++ src/components/CardHolderP/CardHolderP.jsx
@@ -4,6 +4,9 @@ import Card from "../Card/Card";
 import "./CardHolderP.css";
 
 function CardHolderP({
+  data,
+  query,
+  searchKeys,
   selected,
   setSelected,
   setIdofpartimertoshow,
@@ -24,96 +27,49 @@ function CardHolderP({
     console.log(selected);
   }, [selected]);
 
-  const partimersNM = [
-    {
-      id: 0,
-      name: "Ally Mungo",
-      skill1: "catering staff",
-      skill2: "errands",
-      skill3: "driver",
-      rating: 4.9,
-      distance: 2,
-      mobile: "+1 940 925 9956",
-    },
-    {
-      id: 1,
-      name: "Blair Grant",
-      skill1: "customer service",
-      skill2: "errands",
-      skill3: "receptionist",
-      rating: 4.9,
-      distance: 2,
-      mobile: "+1 034 956 0024",
-    },
-    {
-      id: 2,
-      name: "Kalea Ruairi",
-      skill1: "resteraunt staff",
-      skill2: "catering staff",
-      skill3: "sales associate",
-      rating: 4.9,
-      distance: 2,
-      mobile: "+1 304 503 8856",
-    },
-    {
-      id: 3,
-      name: "Edric Seo-Yeon",
-      skill1: "resteraunt staff",
-      skill2: "receptionist",
-      skill3: "driver",
-      rating: 4.9,
-      distance: 2,
-      mobile: "+1 456 263 5583",
-    },
-    {
-      id: 4,
-      name: "Kyle Meredith",
-      skill1: "catering staff",
-      skill2: "cleaning",
-      skill3: "errands",
-      rating: 4.9,
-      distance: 2,
-      mobile: "+1 174 325 3725",
-    },
-  ];
-
   function functionName(partimerNM) {
     setIdofpartimertoshow(partimerNM);
     setShowPartimerProfile(true);
   }
   return (
     <div className="cardHolder__CHP">
-      {partimersNM.map((partimerNM) => (
-        <div className="buttonDiv__CHP" key={partimerNM.id}>
-          <button
-            className="buttonStyle__CHP"
-            onClick={() => functionName(partimerNM)}
-          >
-            <Card
-              name={partimerNM.name}
-              skill1={partimerNM.skill1}
-              skill2={partimerNM.skill2}
-              skill3={partimerNM.skill3}
-              rating={partimerNM.rating}
-              distance={partimerNM.distance}
-            />
-          </button>
-          <button
-            className="buttonStyle__CHP"
-            onClick={() => handleSelect(partimerNM.id)}
-          >
-            <div className="cardSelectorDiv__CHP">
-              <div className="cardSelectorHole__CHP">
-                {selected.indexOf(partimerNM.id) > -1 ? (
-                  <div className="cardSelectorHoleSelected__CHP"></div>
-                ) : (
-                  <></>
-                )}
+      {data
+        // .filter((partimerNM) =>
+        //   searchKeys.some((sKey) =>
+        //     partimerNM[sKey].toLowerCase().includes(query)
+        //   )
+        // )
+        .map((partimerNM) => (
+          <div className="buttonDiv__CHP" key={partimerNM.id}>
+            <button
+              className="buttonStyle__CHP"
+              onClick={() => functionName(partimerNM)}
+            >
+              <Card
+                name={partimerNM.name}
+                skill1={partimerNM.skill1}
+                skill2={partimerNM.skill2}
+                skill3={partimerNM.skill3}
+                rating={partimerNM.rating}
+                distance={partimerNM.distance}
+              />
+            </button>
+            <button
+              className="buttonStyle__CHP"
+              onClick={() => handleSelect(partimerNM.id)}
+            >
+              <div className="cardSelectorDiv__CHP">
+                <div className="cardSelectorHole__CHP">
+                  {selected.indexOf(partimerNM.id) > -1 ? (
+                    <div className="cardSelectorHoleSelected__CHP"></div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
               </div>
-            </div>
-          </button>
-        </div>
-      ))}
+            </button>
+          </div>
+        ))}
     </div>
   );
 }
--- src/pages/BusinessUser/HomeBusiness.jsx
+++ src/pages/BusinessUser/HomeBusiness.jsx
@@ -15,6 +15,61 @@ function HomeBusiness(props) {
   const [showPartimerProfile, setShowPartimerProfile] = useState(false);
   const [showConfirmScreen, setShowConfirmScreen] = useState(false);
   const [idofpartimertoshow, setIdofpartimertoshow] = useState(-1);
+  const [query, setQuery] = useState("");
+  const searchKeys = ["name", "skill1", "skill2", "skill3"];
+
+  const partimersNM = [
+    {
+      id: 0,
+      name: "Ally Mungo",
+      skill1: "catering staff",
+      skill2: "errands",
+      skill3: "driver",
+      rating: 4.9,
+      distance: 2,
+      mobile: "+1 940 925 9956",
+    },
+    {
+      id: 1,
+      name: "Blair Grant",
+      skill1: "customer service",
+      skill2: "errands",
+      skill3: "receptionist",
+      rating: 4.9,
+      distance: 2,
+      mobile: "+1 034 956 0024",
+    },
+    {
+      id: 2,
+      name: "Kalea Ruairi",
+      skill1: "resteraunt staff",
+      skill2: "catering staff",
+      skill3: "sales associate",
+      rating: 4.9,
+      distance: 2,
+      mobile: "+1 304 503 8856",
+    },
+    {
+      id: 3,
+      name: "Edric Seo-Yeon",
+      skill1: "resteraunt staff",
+      skill2: "receptionist",
+      skill3: "driver",
+      rating: 4.9,
+      distance: 2,
+      mobile: "+1 456 263 5583",
+    },
+    {
+      id: 4,
+      name: "Kyle Meredith",
+      skill1: "catering staff",
+      skill2: "cleaning",
+      skill3: "errands",
+      rating: 4.9,
+      distance: 2,
+      mobile: "+1 174 325 3725",
+    },
+  ];
 
   useEffect(() => {
     console.log(idofpartimertoshow);
@@ -23,7 +78,19 @@ function HomeBusiness(props) {
 
   function function2(partimerNM) {
     setShowConfirmScreen(true);
+    console.log(searchFunction(partimersNM));
   }
+
+  const searchFunction = (data) => {
+    return data.filter((item) =>
+      searchKeys.some(
+        (key) =>
+          typeof item[key] === "string" &&
+          item[key].toLowerCase().includes(query)
+      )
+    );
+  };
+
   return (
     <>
       {showPartimerProfile ? (
@@ -67,15 +134,19 @@ function HomeBusiness(props) {
               <form>
                 <textarea
                   name="search"
-                  placeholder="Search"
+                  placeholder=" Search.."
                   className="homesearchb__HB"
                   cols={20}
                   rows={1}
+                  onChange={(e) => setQuery(e.target.value)}
                 />
               </form>
             </div>
             <div className="cardHolderP__HB">
               <CardHolderP
+                data={searchFunction(partimersNM)}
+                query={query}
+                searchKeys={searchKeys}
                 selected={selected}
                 setSelected={setSelected}
                 setIdofpartimertoshow={setIdofpartimertoshow}
